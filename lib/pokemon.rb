cb4f2bb0b88122023661f8c683ca440637a057d1
Berry  Semexan <bsemexan@gmail.com>
2018-02-26 17:43:04 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,13 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
+  @@all = []
+  
   def initialize(name, type, db)
     @name = name
     @type = type
     @db = db
+    @@all << self
   end
 
   def self.save(name, type, db)
